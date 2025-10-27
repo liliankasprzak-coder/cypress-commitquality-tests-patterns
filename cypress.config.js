ea23e92e18d124d77bbdaf9d1914ec856aa824c9
Lilian Raphaele Kasprzak <liliankasprzak@gmail.com>
2025-10-27 13:51:25 -0300

    chore: incluir loja-shopping-patterns no specPattern

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,10 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://commitquality.com',
-    specPattern: 'cypress/e2e/exercicio2_commitquality_tests-patterns/**/*.cy.js',
+    specPattern: [
+      'cypress/e2e/exercicio2_commitquality_tests-patterns/**/*.cy.js',
+      'cypress/e2e/loja-shopping-patterns/**/*.cy.js'
+    ],
     setupNodeEvents(on, config) {
       // eventos/plugins se necessário
       return config;
